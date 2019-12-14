73bf5c861a3652f6f10e627a86015ca34c939448
Isaac Oettinger Struhl <iostruhl@gmail.com>
2019-12-14 17:08:32 -0500

    eh

--- scraper/trailers.py
+++ scraper/trailers.py
@@ -4,16 +4,16 @@ chrome_options = Options()
 chrome_options.add_argument("--headless")
 driver = webdriver.Chrome(options=chrome_options)
 
-with open("movie_list.txt") as f:
+with open("movie_urls.txt") as f:
     with open("trailers.txt", 'w') as w:
         for movie in f:
-            movie = "-".join(movie.rstrip('\n').split(' ')[:-1])
+            movie = movie.rstrip("\n").split("\t")[1]
             print(f"Getting {movie}....", end="")
             url = f"https://www.traileraddict.com/{movie}/trailer"
             driver.get(url)
             try:
                 video_source = driver.find_element_by_id("trailerplayer_html5_api").get_attribute("src")
-                w.write(f"{movie},{video_source}")
+                w.write(f"{movie[1:]},{video_source}")
                 print("success.")
             except:
                 print(f"FAILED.")
