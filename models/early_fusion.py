b2cae7753e497281e5cc31c19faa01551aeb9507
Sidharth Menon <sidharthmenon@dhcp-10-250-76-156.harvard.edu>
2019-12-15 21:15:35 -0500

    cnns added

--- models/early_fusion.py
+++ models/early_fusion.py
@@ -9,15 +9,68 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt
 import seaborn as sns 
 import collections 
+import functools 
+from operator import mul
 
 
 class EarlyFusion(nn.Module): 
-    def __init__(self, input_channels): 
+    def __init__(self, input_channels, input_seq_len, input_sz, embedding_sz=512): 
         super().__init__()
-        self.conv1 = nn.Conv3d(input_channels, 64, kernel_size=())
-    def forward(self, x): 
-        pass
+        assert(input_sz == 64)
+        self.num_output_filters_1 = 64 
+        self.num_output_filters_2 = self.num_output_filters_1 * 2 
+        self.num_output_filters_3a = self.num_output_filters_2 * 2 
+        self.num_output_filters_3b = self.num_output_filters_3a 
+        self.num_output_filters_3c = self.num_output_filters_3b 
 
-# input_data = torch.randn(3, 64, 64, 10) 
-# model = EarlyFusion()
-# print(EarlyFusion(input_data))
+        # Conv Block 1 
+        self.conv1 = nn.Conv3d(input_channels, self.num_output_filters_1, kernel_size=3, stride=1, padding=1)
+        self.norm1 = nn.BatchNorm3d(self.num_output_filters_1)
+        self.maxpool1 = nn.MaxPool3d(kernel_size=(1, 2, 2), stride=(1, 2, 2), padding=0)
+
+        # Conv Block 2 
+        self.conv2 = nn.Conv3d(self.num_output_filters_1, self.num_output_filters_2, kernel_size=(1, 3, 3), stride=1, padding=1)
+        self.norm2 = nn.BatchNorm3d(self.num_output_filters_2)
+        self.maxpool2 = nn.MaxPool3d(kernel_size=(1, 2, 2), stride=(1, 2, 2), padding=0)
+
+        # Conv Block 3 
+        self.conv3a = nn.Conv3d(self.num_output_filters_2, self.num_output_filters_3a, kernel_size=(1, 3, 3), stride=(1, 2, 2), padding=(0, 1, 1)) 
+        # implicit 1x1 convolutions also applied 
+        self.conv3b = nn.Conv3d(self.num_output_filters_3a, self.num_output_filters_3b, kernel_size=(1, 3, 3), stride=1, padding=0)
+        self.conv3c = nn.Conv3d(self.num_output_filters_3a, self.num_output_filters_3c, kernel_size=(1, 3, 3), stride=1, padding=0)
+        self.pool3 = nn.MaxPool3d(kernel_size=(1, 2, 2), stride=(1, 2, 2), padding=0)
+
+        # FC layer 
+        self.embed = nn.Linear(self.num_output_filters_3c * (input_seq_len + 2) * 4, embedding_sz)
+        
+    def forward(self, x): 
+        # (3, 10, 64, 64) -> (64, 4, 64, 64)
+        # Conv Block 1 
+        x = self.conv1(x)
+        x = F.relu(self.norm1(x))
+        x = self.maxpool1(x)
+
+        # Conv Block 2 
+        x = self.conv2(x)
+        x = F.relu(self.norm2(x))
+        x = self.maxpool2(x)
+
+        # Conv Block 3 
+        x = F.relu(self.conv3a(x))
+        x = F.relu(self.conv3b(x))
+        x = F.relu(self.conv3c(x))
+        x = self.pool3(x)
+
+        # Embedding Generation
+        
+        x = x.view((-1, functools.reduce(mul, x.shape[1:])))
+        return torch.tanh(self.embed(x))
+
+
+
+# N x C x T x H x W 
+
+input_data = torch.randn(1, 3, 10, 64, 64) 
+model = EarlyFusion(3, 10, 64)
+outpt = model(input_data)
+print(outpt.shape)
--- models/slow_fusion.py
+++ models/slow_fusion.py
@@ -9,5 +9,70 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt
 import seaborn as sns 
 import collections
+import functools
+from operator import mul
 
-class 
+
+# can only take in 10 frame inputs 
+
+class SlowFusion(nn.Module): 
+    def __init__(self, input_channels, input_seq_len, input_sz, embedding_sz=512): 
+        super().__init__()
+        self.num_output_filters_1 = 16
+        self.num_output_filters_2 = self.num_output_filters_1 * 2
+        self.num_output_filters_3a = self.num_output_filters_2 * 2
+        self.num_output_filters_3b = self.num_output_filters_3a * 2 
+        self.num_output_filters_3c = self.num_output_filters_3b * 2 
+
+        # Conv Block 1 
+        self.conv1 = nn.Conv3d(input_channels, self.num_output_filters_1, kernel_size=(4, 3, 3), stride=(2, 1, 1), padding=(0, 1, 1))
+        self.norm1 = nn.BatchNorm3d(self.num_output_filters_1)
+        self.maxpool1 = nn.MaxPool3d(kernel_size=(1, 2, 2), stride=(1, 2, 2), padding=0) 
+
+        # Conv Block 2 
+        self.conv2 = nn.Conv3d(self.num_output_filters_1, self.num_output_filters_2, kernel_size=(2, 3, 3), stride=(2, 1, 1), padding=(0, 1, 1))
+        self.norm2 = nn.BatchNorm3d(self.num_output_filters_2)
+        self.maxpool2 = nn.MaxPool3d(kernel_size=(1, 2, 2), stride=(1, 2, 2), padding=0)
+
+        # Conv Block 3 
+        self.conv3a = nn.Conv3d(self.num_output_filters_2, self.num_output_filters_3a, kernel_size=(2, 3, 3), stride=1, padding=(0, 1, 1))
+        self.conv3b = nn.Conv2d(self.num_output_filters_3a, self.num_output_filters_3b, kernel_size=1, stride=1, padding=0)
+        self.conv3c = nn.Conv2d(self.num_output_filters_3b, self.num_output_filters_3c, kernel_size=3, stride=1, padding=1)
+        self.pool3 = nn.MaxPool3d(kernel_size=(1, 2, 2), stride=(1, 2, 2), padding=0)
+
+        # FC Layers 
+        self.embedding = nn.Linear(self.num_output_filters_3c * 64, embedding_sz)
+
+        
+    def forward(self, x): 
+        #  Conv Block 1
+        # (3, 10, 64, 64) -> (self.num_output_filters_1, 4, 32, 32)
+        x = self.conv1(x)
+        x = self.norm1(x) 
+        x = F.relu(x)
+        x = self.maxpool1(x)
+
+        # Conv Block 2 
+        # (self.num_output_filters_1, 4, 32, 32) -> (self.num_output_filters_2, 2, 16, 16)
+        x = self.conv2(x)
+        x = self.norm2(x)
+        x = F.relu(x)
+        x = self.maxpool2(x)
+
+        # Conv Block 3 
+        # (self.num_output_filters_2, 2, 16, 16) -> (self.num_output_filters_3, 1, 8, 8)
+        # print(relu_conv3_outpt.size())
+        x = F.relu(self.conv3a(x))
+        x = x.view((-1, x.shape[1], x.shape[3], x.shape[4]))
+        x = F.relu(self.conv3b(x))
+        x = F.relu(self.conv3c(x))
+        x = self.pool3(x)
+        x = x.view(-1, functools.reduce(mul, x.shape[1:])) 
+
+        # Embedding 
+        return torch.tanh(self.embedding(x))
+
+inpt = torch.randn(1, 3, 10, 64, 64) 
+model = SlowFusion(3, 10, 64)
+outpt = model(inpt)
+print(outpt.size())
