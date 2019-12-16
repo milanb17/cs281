0fc2abb7f160c722d23e5fa5bd57cab98e16b52f
Ubuntu <ubuntu@ip-172-31-47-38.ec2.internal>
2019-12-16 23:07:59 +0000

    nice

---
+++ run_more.sh
@@ -0,0 +1,21 @@
+# !/bin/bash
+
+echo "starting larger experiments..."
+
+for fast_seq in lstmn transformer_abs; do 
+    for fast_img in vanilla_cnn slow_fusion early_fusion late_fusion; do 
+        echo "working on timing_SEQ_${fast_seq}_IMG_${fast_img}..."
+        { time python train.py --seq_model $fast_seq --img_model $fast_img --gpu 0; } 2> "./trained_models_new/timing_SEQ_${fast_seq}_IMG_${fast_img}.txt"
+    done 
+done 
+
+echo "done with fast, starting imagenet models..." 
+
+for slow_seq in vanilla_rnn lstm lstmn transformer_abs; do 
+    for slow_img in resnet densenet vgg; do 
+        echo "working on timing_SEQ_${slow_seq}_IMG_${slow_img}..."
+        { time python train.py --seq_model $slow_seq --img_model $slow_img --gpu 0; } 2> "./trained_models_new/timing_SEQ_${slow_seq}_IMG_${slow_img}.txt"
+    done 
+done 
+
+echo "done"
